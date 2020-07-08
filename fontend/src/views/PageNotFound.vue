ea34e391d9dcf8695d0665ad18f46c9d73790822
unknown <tao32040@gmail.com>
2020-07-08 10:08:14 +0700

    แก้ไข css 404 not found
    ปรับให้อยู่กลางหน้าทั้งหมด

--- fontend/src/views/PageNotFound.vue
+++ fontend/src/views/PageNotFound.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="container-fluid">
-      <div class="textnotfound">
-        <H1>404 Page Not Found</H1>
-        <p>The requested path cound not be found</p>
+    <div class="textnotfound">
+      <H1>404 Page Not Found</H1>
+      <p>The requested path cound not be found</p>
     </div>
   </div>
 </template>
 
 <style scoped>
-  .textnotfound{
-padding-top: 300px;
-      margin: auto;
-      text-align: center;
-      vertical-align: middle;
-  }
+.textnotfound {
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transform: translateY(-50%);
+  resize: vertical;
+  overflow: auto;
+  text-align: center;
+}
 </style>
